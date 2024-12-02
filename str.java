c3018fd37d0d2db908c05761af6aa3a021d009ef
23hr1a0455 <kasireddygarihemasree@gmail.com>
2024-12-02 16:05:25 +0530

    Add files via upload

---
+++ str.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class str{
+    public static void main(String args[]){
+        //accepting a string
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b =String.format("congratulations %s you won 5 lackhs",a);
+        System.out.println(b);
+    }
+}
